3c617c2e39545715ec6f1ef35e2705e4b8315186
yogbpl <yogbpl@gmail.com>
2020-04-30 14:21:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-gepecv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-gepecv)
---
+++ index.ts
@@ -0,0 +1,7 @@
+let fruits: string[];
+fruits = ['Apple','Orange','Banana'];
+console.log(fruits);
+
+let food: Array<string>;
+food = ['Pizza','Burger'];
+console.log(food)
